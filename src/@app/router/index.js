45b21466d39984d37d31e7877e5d4236f52f5596
deadpooll312 <deadpollkgz@gmail.com>
2020-05-15 00:08:43 +0600

    Renamed routes folder to wrapper 'cuz it's folder used just for wrapping side bar and route content

--- src/@app/router/index.js
+++ src/@app/router/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
-import Route from './routes';
+import Route from './wrapper';
 
 import HomePage from '../../pages/home';
 
